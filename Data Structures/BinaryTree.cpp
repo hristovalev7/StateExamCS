c2c603fa789f2dc1b05ab2556cc1444272bcc45b
Hristo Valev <hristo.valev7@gmail.com>
2026-06-23 01:14:15 +0300

    Add BinaryTree implementation

---
+++ Data Structures/BinaryTree.cpp
@@ -0,0 +1,80 @@
+#include <stdexcept>
+
+class BinaryTree {
+private:
+    struct Node {
+        int value;
+        Node* left;
+        Node* right;
+
+        Node(int value, Node* left, Node* right) : value(value), left(left), right(right) {}
+    };
+
+    Node* root;
+
+    // left -> right -> root
+    void deallocate(Node* root) {
+        if (root == nullptr) {
+            return;
+        }
+
+        deallocate(root->left);
+        deallocate(root->right);
+        delete root;
+    }
+
+    static Node* copy(Node* root) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+
+        return new Node(root->value, copy(root->left), copy(root->right));
+    }
+
+public:
+    BinaryTree() : root(nullptr) {}
+    BinaryTree(Node* root) : root(copy(root)) {}
+
+    BinaryTree(const BinaryTree& other) : root(copy(other.root)) {}
+
+    BinaryTree& operator=(const BinaryTree& other) {
+        if (this != &other) {
+            deallocate(root);
+            root = copy(other.root);
+        }
+        return *this;
+    }
+
+    ~BinaryTree() {
+        deallocate(root);
+    }
+
+    bool isEmpty() const {
+        return root == nullptr;
+    }
+
+    int getRootValue() const {
+        if (isEmpty()) {
+            throw std::runtime_error("Cannot take the root of an empty tree");
+        }
+
+        return root->value;
+    }
+
+    BinaryTree getLeftSubtree() const {
+        if (isEmpty()) {
+            throw std::runtime_error("Cannot take the left subtree of an empty tree");
+        }
+
+        return BinaryTree(root->left);
+    }
+
+
+    BinaryTree getRightSubtree() const {
+        if (isEmpty()) {
+            throw std::runtime_error("Cannot take the right subtree of an empty tree");
+        }
+
+        return BinaryTree(root->right);
+    }
+};
